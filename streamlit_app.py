b48d7e4288d4f2b9607e904c4563168741813340
thermoscoope <102214216+thermoscoope@users.noreply.github.com>
2026-01-20 13:28:42 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -93,7 +93,7 @@ with header_left:
     try:
         img = Image.open(LOGO_PATH)
         # Skaler ned slik at alt synes (logoen var for høy i tidligere versjoner)
-        st.image(img, width=120)
+        st.image(img, width=360)
     except Exception:
         # Hvis logo mangler i deploy, ikke knekk appen
         st.write("")
