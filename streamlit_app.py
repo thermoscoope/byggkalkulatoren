aab67368786f570261524a373dc056f003acf009
thermoscoope <102214216+thermoscoope@users.noreply.github.com>
2026-01-20 16:09:05 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -108,7 +108,7 @@ with header_right:
         <div class="bk-header-tight">
           <div class="bk-title-row">
             <div class="bk-title"></div>
-            <div class="bk-sub" style="margin-top:10px;">Fra skole til yrke – matematikk tilpasset byggfag</div>
+            <div class="bk-sub" style="margin-top:10px;">Fra skole til yrke – matematikk tilpasset yrkesfag!</div>
           </div>
         </div>
         """,
