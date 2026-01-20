b541b35821bbef0236740a6f3a7b17dab4ee9a3a
thermoscoope <102214216+thermoscoope@users.noreply.github.com>
2026-01-20 15:10:15 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -108,7 +108,7 @@ with header_right:
         <div class="bk-header-tight">
           <div class="bk-title-row">
             <div class="bk-title"></div>
-            <div class="bk-sub" style="margin-top:10px;">– din hjelper i farta</div>
+            <div class="bk-sub" style="margin-top:10px;">Velkommen til din praktiske matematikk hjelper!</div>
           </div>
         </div>
         """,
